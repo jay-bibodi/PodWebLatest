610c1c8fb4f8cc6c6f09aa9c854c932cfdc2b221
Jay Bibodi <35912111+jay-bibodi@users.noreply.github.com>
2018-09-25 22:48:57 -0700

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var regulator = require('./routes/regulator');
+//var regulator = require('./routes/regulator');
 
 var app = express();
 
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/regulator',regulator);
+//app.use('/regulator',regulator);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
